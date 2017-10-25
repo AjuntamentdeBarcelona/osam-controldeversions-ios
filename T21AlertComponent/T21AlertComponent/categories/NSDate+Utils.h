385ef76828c5798c08ffc8d9e7a6f0bacc8de823
Salvador Martín Valero <salvador.martin@atos.net>
2017-10-25 17:25:12 +0200

    First commit

---
+++ T21AlertComponent/T21AlertComponent/categories/NSDate+Utils.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+Utils.h
+//  T21AlertComponent
+//
+//  Created by Tempos21 on 03/07/2017.
+//  Copyright © 2017 Tempos21. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Utils)
+
+- (BOOL)isBetweenDate:(NSDate*)beginDate andDate:(NSDate*)endDate;
+
+@end
---
+++ T21AlertComponent/T21AlertComponent/categories/UIDevice+T21CommonUtils.h
@@ -0,0 +1,22 @@
+//
+//  UIDevice+T21CommonUtils.h
+//  T21Categories/UIDevice/CommonUtils
+//
+//  Created by David Arrufat on 19/05/15.
+//  Copyright (c) 2015 Tempos21. All rights reserved.
+//
+
+#define isiPad UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
+#define isiPhone !isiPad
+
+#define IOS_VERSION_EQUAL_TO(v)          ([[UIDevice currentDevice] systemVersion].floatValue == v)
+#define IOS_VERSION_NEWER_THAN(v)        ([[UIDevice currentDevice] systemVersion].floatValue > v)
+#define IOS_VERSION_NEWER_OR_EQUAL_TO(v) ([[UIDevice currentDevice] systemVersion].floatValue >= v)
+#define IOS_VERSION_OLDER_THAN(v)        ([[UIDevice currentDevice] systemVersion].floatValue < v)
+#define IOS_VERSION_OLDER_OR_EQUAL_TO(v) ([[UIDevice currentDevice] systemVersion].floatValue <= v)
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (T21CommonUtils)
+
+@end
